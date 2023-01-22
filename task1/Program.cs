60a0686f0cf7a72220c771068cb96ed8fe2a18cd
Aleksey <lexandreeev@gmail.com>
2023-01-22 14:03:38 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите номер дня недели:");
+int number = int.Parse(Console.ReadLine()!);
+if(number == 6|number == 7){
+  Console.WriteLine("этот день недели является выходным!");
+}
+else{
+  Console.WriteLine("этот день рабочичй,идите на работу!");
+}
