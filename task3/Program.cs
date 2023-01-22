e53d308744a5b3f4e1a13f255f84f7c87695a342
Aleksey <lexandreeev@gmail.com>
2023-01-22 14:28:56 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+  Console.WriteLine("I четверть");
+else if (x > 0 && y < 0)
+  Console.WriteLine("IV четверть");
+else if (x < 0 && y > 0)
+  Console.WriteLine("II четверть");
+else if (x < 0 && y < 0)
+  Console.WriteLine("III четверть");
+else if (x == 0 || y == 0)
+  Console.WriteLine("Точка является началом координат или лежит на одной из осей x или y");
