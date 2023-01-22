23f035f2332b941b3f76d51c4d75bbcbbfdd484d
Aleksey <lexandreeev@gmail.com>
2023-01-22 15:33:52 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿double x1 = 50;
+double y1 = 20;
+double z1 = 30;
+double x2 = 100;
+double y2 = 45;
+double z2 = 87;
+double result =  Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine(result);
+double result2 =  Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)+ Math.Pow(z2 - z1,2));
+Console.WriteLine(result2);
+
+
+
